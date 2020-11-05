a7771d97dd5efa2e488228b9545632a4d80a7df6
Steven Normore <snormore@gmail.com>
2020-11-05 09:45:06 -0500

    Wait for deployment to complete

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,25 @@ APP_ID="$(doctl app list -ojson | jq -r "${JQ_ARGS}")"
 doctl app get ${APP_ID} -ojson | yq r -P - '[0].spec' > /tmp/app.actual.yaml
 cat /tmp/app.actual.yaml
 if ! diff /tmp/app.actual.yaml .do/app.yaml; then
-    echo "Updating app spec from .do/app.yaml"
+    echo "=> Updating app spec from .do/app.yaml"
     doctl app update ${APP_ID} --spec .do/app.yaml
 fi
 
 # Trigger a deployment using the app ID.
-echo "Deploying app ${APP_NAME} (${APP_ID})..."
+echo "=> Deploying app ${APP_NAME} (${APP_ID})..."
 doctl app create-deployment $APP_ID
+
+# Wait for latest deployment to be active or failed.
+while true; do
+    PHASE="$(doctl app list-deployments ${APP_ID} -ojson | jq -r '.[0].phase')"
+    echo "-- Phase=${PHASE}"
+    if [ "${PHASE}" == "ACTIVE" ]; then
+        echo "=> Success! 🎉"
+        exit 0
+    fi
+    if [ "${PHASE}" == "FAILED" ]; then
+        echo "=> Failure, check your app deploy logs. 🚨"
+        exit 1
+    fi
+    sleep 3
+done
